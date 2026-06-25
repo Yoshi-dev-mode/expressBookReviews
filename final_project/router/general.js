39fe447de0013d8412241d14be629fc5ba34c748
Evan <yoshi@gmail.com>
2026-06-25 11:27:32 +0800

    Added Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require("axios");
 
 public_users.post("/register", (req,res)=>{
 
@@ -96,6 +96,50 @@ public_users.get('/review/:isbn', function(req,res){
     return res.status(200).json(
         books[isbn].reviews
     );
+
+});
+
+// Task 10 - Get all books using Async/Await
+public_users.get('/async/books', async function (req, res) {
+    try {
+        const response = await axios.get("http://localhost:5000/");
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({
+            message: "Unable to fetch books"
+        });
+    }
+});
+
+// Task 12 - Get books by Author using Async/Await
+public_users.get('/async/author/:author', async function (req, res) {
+
+    const author = req.params.author;
+
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(404).json({
+            message: "Author not found"
+        });
+    }
+
+});
+
+// Task 13 - Get books by Title using Async/Await
+public_users.get('/async/title/:title', async function (req, res) {
+
+    const title = req.params.title;
+
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(404).json({
+            message: "Title not found"
+        });
+    }
 
 });
 
